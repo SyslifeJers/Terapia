295ea70480cbc95766df08121f4e7e2113405762
Juan Elias Romero Sanchez <42285286+SyslifeJers@users.noreply.github.com>
2025-07-12 22:17:25 -0600

    Simplify root welcome page

---
+++ html/includes/footer.php
@@ -0,0 +1,7 @@
+    <!-- JavaScript -->
+    <script src="./assets/js/bundle.js?ver=3.3.0"></script>
+    <script src="./assets/js/scripts.js?ver=3.3.0"></script>
+    <script src="./assets/js/charts/gd-campaign.js?ver=3.3.0"></script>
+</body>
+
+</html>
---
+++ html/includes/head.php
@@ -0,0 +1,18 @@
+<?php $baseHref = isset($baseHref) ? $baseHref : '../'; ?>
+<!DOCTYPE html>
+<html lang="zxx" class="js">
+
+<head>
+    <base href="<?php echo $baseHref; ?>">
+    <meta charset="utf-8">
+    <meta name="author" content="Softnio">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="A powerful and conceptual apps base dashboard template that especially build for developers and programmers.">
+    <!-- Fav Icon  -->
+    <link rel="shortcut icon" href="./images/favicon.png">
+    <!-- Page Title  -->
+    <title>Dashboard - Campaign | DashLite Admin Template</title>
+    <!-- StyleSheets  -->
+    <link rel="stylesheet" href="./assets/css/dashlite.css?ver=3.3.0">
+    <link id="skin-default" rel="stylesheet" href="./assets/css/theme.css?ver=3.3.0">
+</head>
